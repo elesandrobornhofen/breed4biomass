35beb0474cca941f06f46672acfe2ffef53b4c0e
elesandrobornhofen <elesandro.bornhofen@gmail.com>
2022-12-14 19:45:11 +0100

    documenting data sets.

--- R/RNA.R
+++ R/RNA.R
@@ -1,14 +1,16 @@
-#' Data of RNAseq-based gene expression
+#' Gene expression data via RNA sequencing
 #'
-#' A dataset containing NMR variables obtained by processing the spectra with a bucketing algorithm 
-#' (De Meyer et al., 2008). The data represents the area within each bin. All variables were 
-#' mean-centered and Pareto scaled. There are 144 samples, which consists of 79 F2 families 
+#' A dataset containing gene expression data for 4,767 genes. Reads were mapped to the 
+#' *Lolium perenne* reference genome (Nagy et al., 2022). The expression matrix in normalized read count 
+#' values in fragments per kilobase of transcript per million (FPKM) was filtered by expression
+#' median, variance, and read counts across samples. Finally, a pseudo count was added and the log base 2
+#' calculated $$[log_2(x+1)]$$. There are 144 samples, which consists of 79 F2 families 
 #' of hybrid ryegrass (samples b001 to b079) and 65 F2 families of \emph{Festulolium loliaceum}
 #' (samples b086 to b150).
 #' 
 #'
-#' @format A data frame with 556 rows and 144 variables.
+#' @format A data frame with 4767 rows and 144 variables.
 #' @source \url{ insert here the URL to the preprint }
-#' @references De Meyer et al. (2008) Anal. Chem. 2008, 80, 10, 3783–3790
-#' (\href{https://doi.org/10.1021/ac7025964}{doi.})
+#' @references Nagy et al. (2022) BMC Genomics 23, 505.
+#' (\href{https://doi.org/10.1186/s12864-022-08697-0}{doi.})
 "RNA"
--- README.md
+++ README.md
@@ -5,9 +5,7 @@
 <!-- badges: end -->
 
 The goal of breed4biomass package is to make multi-omic as well as phenotypic data recorded in the 
-Breed for Biomass project ( grant ) readily available to the scientific community. The package contains
-data of nuclear magnetic resonance spectroscopy, RNA sequencing, and SNP-based genomic markers for 144 
-individuals. Samples are represented by family pools of hybrid ryegrass (79 entries) and 
+Breed for Biomass project (grant 6150-00020B, Innovation Fund Denmark) readily available to the scientific community. The package contains data of nuclear magnetic resonance spectroscopy, RNA sequencing, and SNP-based genomic markers for 144  individuals. Samples are represented by family pools of hybrid ryegrass (79 entries) and 
 *Festulolium loliaceum* (65 entries).
 
 
